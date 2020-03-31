ccea68d8dded00037f7d7aaf1c98d06e4535014f
chidchanok.buj <chidchanok.buj@gmail.com>
2020-03-31 21:03:46 +0700

    Trying to add RecyclerView

---
+++ app/src/main/java/com/wireless/stickie/ShuffleOSI.kt
@@ -0,0 +1,13 @@
+package com.wireless.stickie
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+
+class ShuffleOSI : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shuffle_o_s_i)
+    }
+}
