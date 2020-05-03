bdc784a177ac756c312de6c2628b385518f0c41b
chidchanok.buj <chidchanok.buj@gmail.com>
2020-05-03 23:54:44 +0700

    Edit Minor Bugs

--- app/src/main/java/com/wireless/stickie/CongestionLearning.kt
+++ app/src/main/java/com/wireless/stickie/CongestionLearning.kt
@@ -2,11 +2,15 @@ package com.wireless.stickie
 
 import android.animation.ArgbEvaluator
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.viewpager.widget.ViewPager
 import androidx.viewpager.widget.ViewPager.OnPageChangeListener
 import com.wireless.stickie.Adapter.CardAdapter
 import com.wireless.stickie.Model.ModelCard
+import java.util.*
 import kotlin.collections.ArrayList
 
 class CongestionLearning : AppCompatActivity() {
@@ -22,14 +26,7 @@ class CongestionLearning : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_card_view)
         modelCards = ArrayList()
-        modelCards.add(ModelCard(R.drawable.solution, getString(R.string.model_layer_topic), ""))
-        modelCards.add(
-            ModelCard(
-                R.drawable.empty,
-                getString(R.string.congestion),
-                getString(R.string.congestionDesc)
-            )
-        )
+        modelCards.add(ModelCard(R.drawable.empty, getString(R.string.congestion_control_topic), ""))
         modelCards.add(
             ModelCard(
                 R.drawable.empty,
@@ -91,5 +88,49 @@ class CongestionLearning : AppCompatActivity() {
             override fun onPageSelected(position: Int) {}
             override fun onPageScrollStateChanged(state: Int) {}
         })
+    }
+    private fun showChangeLang() {
+        val listItems = arrayOf("ภาษาไทย","English")
+        val mBuilder = AlertDialog.Builder(this@CongestionLearning)
+        mBuilder.setTitle("Choose Language")
+        mBuilder.setSingleChoiceItems(listItems, -1) { dialog, which ->
+            if (which == 0) {
+                setLocate("th")
+                recreate()
+            } else if (which == 1) {
+                setLocate("en")
+                recreate()
+            }
+            dialog.dismiss()
+        }
+
+        val mDialog = mBuilder.create()
+
+        mDialog.show()
+    }
+
+    private fun setLocate(Lang: String?) {
+        val config = resources.configuration
+        val locale = Locale(Lang)
+
+        Locale.setDefault(locale)
+        config.locale = locale
+        resources.updateConfiguration(config, resources.displayMetrics)
+
+        recreate()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_lang -> {
+                showChangeLang()
+            }
+        }
+        return true
     }
 }
